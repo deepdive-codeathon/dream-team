c525eb666111d51a9dece365e7f1944a3cb9f95f
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-10-05 15:48:20 -0600

    added image to splash

--- app/src/components/splash.jsx
+++ app/src/components/splash.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from "react";
 import img from "/src/shared/img/Logo";
+import "./App.css";
+
 
 export const Splash = () => {
 	return (
 		<>
-
+			<img src={img} className="img-fluid" id="logo" alt="logo"/>
 		</>
 	)
 };
