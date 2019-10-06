7cb5f28581772213d8f2e9f25e4785f0f66ee16b
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-10-06 09:20:23 -0600

    fixed bootstrap col

--- app/src/components/ThisOrThat.jsx
+++ app/src/components/ThisOrThat.jsx
@@ -1,38 +1,33 @@
 import React from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap/';
 import ApiRandomizer from '../shared/ApiRandomizer.jsx';
+import img from "../shared/img/Logo.png";
+import image from "../shared/img/question.png"
 
 /**
  * This is that main component that makes up ThisOrThat.
  */
-export default class ThisOrThat extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export const ThisOrThat = () => {
 
-    handlClick = () => {
-        console.log("WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-    }
-
-    render() {
+   
         return (
-            <>
+           <>
+               <img src={img} className="img-fluid" id="logo" alt="logo" />
+            <Container className="this-or-that">
 
                 <Row>
-                    <Col>
-                        <div>
+                    <Col lg={4} className="randomizer">
                             <ApiRandomizer />
-                        </div>
                     </Col>
-                    <Col></Col>
-                    <Col>
-                        <div>
+                    <Col lg={2}>
+                        <img src={image} className="img-fluid" id="question" alt="question"/>
+                    </Col>
+                    <Col lg={4} className="randomizer">
                             <ApiRandomizer />
-                        </div>
                     </Col>
                 </Row>
-
-            </>
+            </Container>
+           </>
         );
-    }
-}
+};
+export default ThisOrThat;
